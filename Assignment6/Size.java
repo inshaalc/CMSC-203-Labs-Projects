a3cb907b500523b740a8a0589d3b86c9784ed328
Inshaal <139726478+inshaalc@users.noreply.github.com>
2024-08-06 11:51:52 -0400

    Create Size.java

---
+++ Assignment6/Size.java
@@ -0,0 +1,17 @@
+/*
+ * Class: CMSC203 
+ * Instructor: Grigoriy Grinberg
+ * Description: Enum constants
+ * Due: 08/05/2024
+ * Platform/compiler: Eclipse
+ * I pledge that I have completed the programming  
+ * assignment independently. I have not copied the code 
+ * from a student or any source. I have not given my code 
+ * to any student.
+   Print your Name here: Inshaal Chaudhury
+*/
+
+public enum Size
+{
+	SMALL, MEDIUM, LARGE;
+}
